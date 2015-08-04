dec3a53a6a5b5a72c2f2217d1649c59ab22c9c10
ruilinchu <ruilinchu@gmail.com>
2015-08-04 17:12:05 -0400

    Update sync_user.sh

--- sync_user.sh
+++ sync_user.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 ## this is unnecessary, use wwuseradd username -p password instead
+useradd $@
+su $1
+logout
 wwsh -y file import /etc/passwd
 wwsh -y file import /etc/group
-wwsh -y provision set --fileadd passwd,group
+wwsh -y file import /etc/shadow
+wwsh -y provision set --fileadd passwd,group,shadow
 wwsh file sync
