a97d6fd3ededeccdce9276bf8211dc2a932ecb3a
ruilinchu <ruilinchu@gmail.com>
2015-08-03 21:47:31 -0400

    add

--- ww_setup.sh
+++ ww_setup.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+echo making chroot ... 
 wwmkchroot centos-6 /var/chroots/centos-6
+echo warewulf initiating
 CHROOTDIR=/var/chroots/centos-6 wwinit ALL
+echo scanning and adding nodes
+wwnodescan --netdev=eth0 --ipaddr=172.16.0.1 --netmask=255.255.0.0 --vnfs=centos-6 --bootstrap=`uname -r` --groups=newnodes n[01-10]
+
