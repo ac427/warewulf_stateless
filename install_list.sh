63b6a76c381f1e5523fa592c26370ed9c754f6fa
ruilinchu <ruilinchu@gmail.com>
2015-08-03 23:06:30 -0400

    add

--- install_list.sh
+++ install_list.sh
@@ -3,8 +3,6 @@
 yum groupinstall "Development tools"
 yum groupinstall "MySQL Database server"
 yum install httpd dhcp tftp-server mod_perl wget tcpdump emacs nfs-utils ntp man git
-wget http://warewulf.lbl.gov/downloads/repo/warewulf-rhel6.repo -O /etc/yum.repos.d/warewulf-rhel6.repo
-yum install warewulf-provision warewulf-cluster warewulf-provision-server warewulf-vnfs 
 wget http://mirrors.mit.edu/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -Uvh epel-release-6-8.noarch.rpm
 yum install pdsh
--- ww_setup.sh
+++ ww_setup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-echo making chroot ... 
+echo downloading and installing warewulf
+wget http://warewulf.lbl.gov/downloads/repo/warewulf-rhel6.repo -O /etc/yum.repos.d/warewulf-rhel6.repo
+yum install warewulf-provision warewulf-cluster warewulf-provision-server warewulf-vnfs 
+echo warewulf making chroot ... 
 wwmkchroot centos-6 /var/chroots/centos-6
 echo warewulf initiating
 CHROOTDIR=/var/chroots/centos-6 wwinit ALL
-echo scanning and adding nodes
+echo warewulf scanning and adding nodes
 wwnodescan --netdev=eth0 --ipaddr=172.16.0.1 --netmask=255.255.0.0 --vnfs=centos-6 --bootstrap=`uname -r` --groups=newnodes n[01-10]
 
