f666a44e2b804ed28781d54c0e9b97066a8db90b
ruilinchu <ruilinchu@gmail.com>
2015-08-05 14:35:55 -0400

    Update add_ssh_key.sh

--- add_ssh_key.sh
+++ add_ssh_key.sh
@@ -6,8 +6,9 @@ done > /etc/ssh/ssh_known_hosts
 
 cp /etc/ssh/ssh_known_hosts /var/chroots/centos-6/etc/
 
-## rebuild image or sync file to take effect on compute nodes
+## rebuild image and reboot compute nodes or sync file to take effect on compute nodes
 ## wwvnfs --chroot /var/chroots/centos-6
+## pdsh -w `grep eth0 /etc/hosts | awk '{print $1}' | paste -d, -s ` reboot
 
 wwsh -y file import /etc/ssh/ssh_known_hosts
 wwsh -y provision set --fileadd ssh_known_hosts
