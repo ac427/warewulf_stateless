fd871fc2f7b704e8e716511f6e6285077d786e9d
ruilinchu <ruilinchu@gmail.com>
2015-08-05 18:13:56 -0400

    Create compute_setup.sh

---
+++ compute_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## install gcc to compute nodes 
+yum --tolerant --installroot /var/chroots/centos-6 -y install gdb gcc gcc-c++ gcc-gfortran
+
+## rebuild image 
+wwvnfs -y --chroot /var/chroots/centos-6
