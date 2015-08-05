94b22f814171164ec11fb519f7071949d40e6537
ruilinchu <ruilinchu@gmail.com>
2015-08-05 18:26:21 -0400

    Update 05_compute_setup.sh

--- 05_compute_setup.sh
+++ 05_compute_setup.sh
@@ -3,5 +3,9 @@
 ## install gcc to compute nodes 
 yum --tolerant --installroot /var/chroots/centos-6 -y install gdb gcc gcc-c++ gcc-gfortran
 
+## install Lmod
+
+## 
+
 ## rebuild image 
 #wwvnfs -y --chroot /var/chroots/centos-6
