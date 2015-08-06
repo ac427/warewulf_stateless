d5a6b8c6ca41950c918836201c5a230803384f81
ruilinchu <ruilinchu@gmail.com>
2015-08-06 17:59:07 -0400

    Update 06_Lmod.sh

--- 06_Lmod.sh
+++ 06_Lmod.sh
@@ -19,6 +19,28 @@ make install
 ln -s /opt/apps/lmod/lmod/init/profile /etc/profile.d/modules.sh
 ln -s /opt/apps/lmod/lmod/init/cshrc /etc/profile.d/modules.csh
 
+## standard set of module
+cat > /opt/apps/modulefiles/Core/StdEnv.lua << EOF
+load("")
+EOF
+
+cat > /etc/profile.d/z00_StdEnv.sh << EOF
+if [ -z "$__Init_Default_Modules" -o -z "$LD_LIBRARY_PATH" ]; then
+  __Init_Default_Modules=1; export __Init_Default_Modules;
+  module getdefault default || module load StdEnv
+fi
+EOF
+
+cat > /etc/profile.d/z00_StdEnv.csh << EOF
+if ( ! $?__Init_Default_Modules || ! $?LD_LIBRARY_PATH ) then
+  module getdefault default
+  if ( $status != 0 ) then
+    module load StdEnv
+  endif
+  setenv __Init_Default_Modules 1
+endif
+EOF
 cp /etc/profile.d/modules.* /var/chroots/centos-6/etc/profile.d
+cp /etc/profile.d/z00_.* /var/chroots/centos-6/etc/profile.d
 
 ## rebuild image
