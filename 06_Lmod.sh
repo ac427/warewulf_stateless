92cd524a1588c8de5b4b0aea95268a0051ab27ec
ruilinchu <ruilinchu@gmail.com>
2015-08-06 16:27:24 -0400

    Update 06_Lmod.sh

--- 06_Lmod.sh
+++ 06_Lmod.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-## system have an old lua 5.1, anyway, let do the latest
-git clone https://github.com/LuaDist/lua
-cd lua
-cmake .
+## system have an old lua without posix and lfs module 
+wget http://sourceforge.net/projects/lmod/files/lua-5.1.4.8.tar.gz/download
+tar xvzf download
+cd lua-5.1.4.8
+./configure --prefix=/opt/apps/lua/5.1.4
 make
-mkdir -p /opt/apps/lua/bin
-mkdir -p /opt/apps/lua/lib
-cp lua luac /opt/apps/lua/bin
-cp liblua.so /opt/apps/lua/lib
+make install
+ln -s /opt/apps/lua/5.1.4 /opt/apps/lua/lua
+ln -s /opt/apps/lua/lua/bin/lua /usr/local/bin
 
 ## lmod
 git clone https://github.com/TACC/Lmod
+cd Lmod
+./configure --prefix=/opt/apps
+make install
