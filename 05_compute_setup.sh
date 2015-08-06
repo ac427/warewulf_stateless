72eae8a9956804c0165d097f36945a157701ca8a
ruilinchu <ruilinchu@gmail.com>
2015-08-06 13:05:40 -0400

    Update 05_compute_setup.sh

--- 05_compute_setup.sh
+++ 05_compute_setup.sh
@@ -5,11 +5,17 @@ yum --tolerant --installroot /var/chroots/centos-6 -y install `paste -s compute_
 
 ## configure munge
 create-munge-key
+chkconfig munge on
 service munge start
 echo testing  munge ...
 munge -n | unmunge 
 
 cp /etc/munge/munge.key /var/chroots/centos-6/etc/munge/munge.key
+
+cat >> /var/chroots/centos-6/etc/rc.local << EOF
+chkconfig munge on
+service munge start
+EOF
 
 ## install Lmod in /opt, copy script to /etc and image /etc
 
