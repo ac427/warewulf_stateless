e0b09c4dfc6cf09fc946f89c1f4515a1e139c52b
ruilinchu <ruilinchu@gmail.com>
2015-08-07 15:10:08 -0400

    Update 06_mpi_slurm.sh

--- 06_mpi_slurm.sh
+++ 06_mpi_slurm.sh
@@ -3,14 +3,10 @@
 ## new gcc
 yum install gmp-devel mpfr-devel libmpc-devel
 wget http://mirrors.concertpass.com/gcc/releases/gcc-5.2.0/gcc-5.2.0.tar.gz
-tar xvzf gcc-5.2.0.tar.gz
 
-cd gcc-5.2.0
-mkdir build
-cd build
-../configure --enable-shared --prefix=/opt/apps/gcc/5.2.0 --disable-multilib --disable-bootstrap
-make -j12
-make install
+## rpmbuild using gcc spec file
+
+
 
 ## slurm
 
