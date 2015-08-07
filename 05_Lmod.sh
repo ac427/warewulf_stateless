036acedd81fc5d83ddfa620a8b75701949650fae
ruilinchu <ruilinchu@gmail.com>
2015-08-07 12:49:25 -0400

    Update 05_Lmod.sh

--- 05_Lmod.sh
+++ 05_Lmod.sh
@@ -49,6 +49,4 @@ cp /etc/profile.d/modules.* /var/chroots/centos-6/etc/profile.d
 cp /etc/profile.d/z00_* /var/chroots/centos-6/etc/profile.d
 
 ## reimage vnfs
-wwvnfs -y --chroot /var/chroots/centos-6
-## reboot all compute nodes
-pdsh -w `grep eth0 /etc/hosts | awk '{print $1}' | paste -d, -s ` reboot
+./reim_reboot
