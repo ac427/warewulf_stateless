e9909057504ad6381820113e919163a9934cab85
ruilinchu <ruilinchu@gmail.com>
2015-08-07 14:16:39 -0400

    Update 02_ww_setup.sh

--- 02_ww_setup.sh
+++ 02_ww_setup.sh
@@ -8,9 +8,11 @@ wwmkchroot centos-6 /var/chroots/centos-6
 #echo warewulf initiating ...
 #CHROOTDIR=/var/chroots/centos-6 wwinit ALL
 wwvnfs --chroot /var/chroots/centos-6
-
-echo warewulf scanning and adding nodes ...
-echo please bring up the compute nodes one by one in order to PXE boot now
+echo =
+echo =
+echo =
+echo warewulf scanning and adding nodes 
+echo Now please bring up the compute nodes one by one in order 
 sleep 3
 wwnodescan --netdev=eth0 --ipaddr=172.16.0.1 --netmask=255.255.0.0 --vnfs=centos-6 --bootstrap=`uname -r` --groups=newnodes n[01-10]
 
