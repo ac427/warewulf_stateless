cbc658e5e281f44f94fb3e2b0033b9548313dbec
ruilinchu <ruilinchu@gmail.com>
2015-08-06 23:50:51 -0400

    Update 01_install_list.sh

--- 01_install_list.sh
+++ 01_install_list.sh
@@ -2,7 +2,7 @@
 ##install list on provision node
 yum -y groupinstall "Development tools"
 yum -y groupinstall "MySQL Database server"
-yum -y install httpd dhcp tftp-server mod_perl wget tcpdump nfs-utils ntp man 
+yum -y install httpd dhcp tftp-server mod_perl wget tcpdump nfs-utils ntp man finger
 ## emacs git
 wget http://mirrors.mit.edu/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -Uvh epel-release-6-8.noarch.rpm
