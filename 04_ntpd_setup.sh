a9d5d8e51b95b6663d97127804de987b37ccd06b
ruilinchu <ruilinchu@gmail.com>
2015-08-06 23:57:05 -0400

    Update 04_ntpd_setup.sh

--- 04_ntpd_setup.sh
+++ 04_ntpd_setup.sh
@@ -17,5 +17,7 @@ chkconfig ntpd on
 service ntpd start
 EOF
 
-#wwvnfs -y --chroot /var/chroots/centos-6
-#pdsh -w `grep eth0 /etc/hosts | awk '{print $1}' | paste -d, -s ` reboot
+./add_user chu opera1122
+
+##wwvnfs -y --chroot /var/chroots/centos-6
+##pdsh -w `grep eth0 /etc/hosts | awk '{print $1}' | paste -d, -s ` reboot
