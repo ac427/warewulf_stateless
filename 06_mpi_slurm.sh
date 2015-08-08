b3565a16f0bcc0d1b11f35e23bbd12e1849a63a2
ruilinchu <ruilinchu@gmail.com>
2015-08-07 23:58:18 -0400

    Update 06_mpi_slurm.sh

--- 06_mpi_slurm.sh
+++ 06_mpi_slurm.sh
@@ -11,8 +11,14 @@ cp rpm_specs/* ~/rpmbuild/SPECS
 rpmbuild -ba ~/rpmbuild/SPECS/gcc-5.2.0.spec
 
 ## slurm
+wget -P ~/rpmbuild/SOURCES https://github.com/SchedMD/slurm/archive/slurm-14-11-8-1.tar.gz
+cd ~/rpmbuild/SOURCES
+tar xvzf slurm-14-11-8-1.tar.gz
+cd slurm-slurm-14-11-8-1
+./configure --prefix=/opt/slurm/14.11.8 --sysconfdir=/opt/slurm/14.11.8/etc
 
 ## openmpi, configed to do srun in slurm
-
+wget -P ~/rpmbuild/SOURCES http://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.8.tar.gz
+rpmbuild -ba ~/rpmbuild/SPECS/openmpi-1.8.8.spec
 
 ## now we can kick start the app stack and module tree
