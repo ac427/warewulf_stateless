51573413f9054b20dbdaf28ef3f2e888f465af85
ruilinchu <ruilinchu@gmail.com>
2015-08-07 23:13:04 -0400

    Update 06_mpi_slurm.sh

--- 06_mpi_slurm.sh
+++ 06_mpi_slurm.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-
 yum install gmp-devel mpfr-devel libmpc-devel rpm-build redhat-rpm-config
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
 
-wget http://mirrors.concertpass.com/gcc/releases/gcc-5.2.0/gcc-5.2.0.tar.gz
-
+## new gcc
+wget ‐P ~/rpmbuild/SOURCES http://mirrors.concertpass.com/gcc/releases/gcc-5.2.0/gcc-5.2.0.tar.gz
+cp rpm_specs/* ~/rpmbuild/SPECS
 ## rpmbuild using gcc spec file
-
-
+rpmbuild -ba ~/rpmbuild/SPECS/gcc-5.2.0.spec
 
 ## slurm
 
