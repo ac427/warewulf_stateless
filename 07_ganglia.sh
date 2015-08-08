471c1fc747cd4ac70dee283c87c5d1db8287a5c6
ruilinchu <ruilinchu@gmail.com>
2015-08-08 00:48:06 -0400

    Update 07_ganglia.sh

--- 07_ganglia.sh
+++ 07_ganglia.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 yum -y install libganglia ganglia-gmond ganglia-gmetad ganglia-web
+chkconfig gmetad on
+chkconfig gmond on
 
+service gmond start
+service gmetad start
+
+yum --tolerant --installroot /var/chroots/centos-6 -y install libganglia ganglia-gmond
+
+## edit the two /etc/ganglia/gmond.conf files
