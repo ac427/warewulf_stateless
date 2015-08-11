0bd56551933232e2ed49428409259981f7b211b8
ruilinchu <ruilinchu@gmail.com>
2015-08-11 14:30:52 -0400

    Update 07_openmpi.sh

--- 07_openmpi.sh
+++ 07_openmpi.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+## new gcc
+wget ‐P ~/rpmbuild/SOURCES http://mirrors.concertpass.com/gcc/releases/gcc-5.2.0/gcc-5.2.0.tar.gz
+cp rpm_specs/* ~/rpmbuild/SPECS
+## rpmbuild using gcc spec file, replace the HMS with your own name in SPEC file
+rpmbuild -ba ~/rpmbuild/SPECS/gcc-5.2.0.spec
+##rpm -ivh ~/rpmbuild/RPMS/x86_64/gcc-5.2.*.rpm
+
+## openmpi, configed to do srun in slurm
+wget -P ~/rpmbuild/SOURCES http://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-1.8.8.tar.gz
+rpmbuild -ba ~/rpmbuild/SPECS/openmpi-1.8.8.spec
+##rpm -ivh ~/rpmbuild/RPMS/x86_64/openmpi-1.8*.rpm
+## now we can kick start the app stack and module tree
