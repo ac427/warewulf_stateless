08914214c43b166b55a9c8e1dc76c26c752a2467
ruilinchu <ruilinchu@gmail.com>
2015-08-11 17:54:25 -0400

    Update 06_slurm.sh

--- 06_slurm.sh
+++ 06_slurm.sh
@@ -70,7 +70,7 @@ mysqladmin -u root password 'password'
 mysql -u root -ppassword << EOF
 create database slurm_acct_db;
 create user 'slurm'@'localhost';
-set password for 'slurm'@'localhost' = password('MyStoragePassword');
+set password for 'slurm'@'localhost' = password('password');
 grant usage on *.* to 'slurm'@'localhost';
 grant all privileges on slurm_acct_db.* to 'slurm'@'localhost';
 flush privileges;
