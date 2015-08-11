dd5e4228ea035a1334d18a56a103ea4f76138d98
ruilinchu <ruilinchu@gmail.com>
2015-08-11 13:06:57 -0400

    Update 06_mpi_slurm.sh

--- 06_mpi_slurm.sh
+++ 06_mpi_slurm.sh
@@ -4,16 +4,11 @@ yum install gmp-devel mpfr-devel libmpc-devel rpm-build redhat-rpm-config
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
 
-## new gcc
-wget ‐P ~/rpmbuild/SOURCES http://mirrors.concertpass.com/gcc/releases/gcc-5.2.0/gcc-5.2.0.tar.gz
-cp rpm_specs/* ~/rpmbuild/SPECS
-## rpmbuild using gcc spec file, replace the HMS with your own name in SPEC file
-rpmbuild -ba ~/rpmbuild/SPECS/gcc-5.2.0.spec
-##rpm -ivh ~/rpmbuild/RPMS/x86_64/gcc-5.2.*.rpm
-
 ## slurm
 wget -P ~/rpmbuild/SOURCES https://github.com/SchedMD/slurm/archive/slurm-14-11-8-1.tar.gz
 cd ~/rpmbuild/SOURCES
+## you can also do rpmbuild -ta slurm*.gz and rpmbuild -install the rpm
+## configuration guide can also be figured out from the slurm.spec file inside tar file
 tar xvzf slurm-14-11-8-1.tar.gz
 cd slurm-slurm-14-11-8-1
 ./configure --prefix=/opt/slurm/14.11.8 --sysconfdir=/opt/slurm/14.11.8/etc
@@ -22,6 +17,14 @@ make install
 ## sample scripts for deamon service and configuration
 cp -r etc /opt/slurm/14.11.8/
 ##start slurmd on compute nodes and slurmctld on master node on boot
+
+## new gcc
+wget ‐P ~/rpmbuild/SOURCES http://mirrors.concertpass.com/gcc/releases/gcc-5.2.0/gcc-5.2.0.tar.gz
+cp rpm_specs/* ~/rpmbuild/SPECS
+## rpmbuild using gcc spec file, replace the HMS with your own name in SPEC file
+rpmbuild -ba ~/rpmbuild/SPECS/gcc-5.2.0.spec
+##rpm -ivh ~/rpmbuild/RPMS/x86_64/gcc-5.2.*.rpm
+
 
 ## you will need to configure file slurm.conf using the html tool in doc folder under install directory
 
