c831e9e6a56168f3327604ba74ac2232dadf04e0
ruilinchu <ruilinchu@gmail.com>
2015-08-11 14:23:40 -0400

    Update 06_mpi_slurm.sh

--- 06_mpi_slurm.sh
+++ 06_mpi_slurm.sh
@@ -16,8 +16,34 @@ cd slurm-slurm-14-11-8-1
 make 
 make install
 ## sample scripts for deamon service and configuration
-cp -r etc /opt/slurm/14.11.8/
+##cp -r etc /opt/slurm/14.11.8/
+if [ -d /etc/init.d ]; then
+  install -D -m755 etc/init.d.slurm    /etc/init.d/slurm
+  install -D -m755 etc/init.d.slurmdbd /etc/init.d/slurmdbd
+  mkdir -p "/usr/sbin"
+  ln -s ../../etc/init.d/slurm    /usr/sbin/rcslurm
+  ln -s ../../etc/init.d/slurmdbd /usr/sbin/rcslurmdbd
+fi
+## this is for centos-7
+if [ -d /usr/lib/systemd/system ]; then
+  install -D -m755 etc/slurmctld.service /usr/lib/systemd/system/slurmctld.service
+  install -D -m755 etc/slurmd.service    /usr/lib/systemd/system/slurmd.service
+  install -D -m755 etc/slurmdbd.service  /usr/lib/systemd/system/slurmdbd.service
+fi
+
+install -D -m644 etc/slurm.conf.example /opt/slurm/14.11.8/etc/slurm.conf.example
+install -D -m644 etc/cgroup.conf.example /opt/slurm/14.11.8/etc/cgroup.conf.example
+install -D -m644 etc/cgroup_allowed_devices_file.conf.example /opt/slurm/14.11.8/etc/cgroup_allowed_devices_file.conf.example
+install -D -m755 etc/cgroup.release_common.example /opt/slurm/14.11.8/etc/cgroup.release_common.example
+install -D -m755 etc/cgroup.release_common.example /opt/slurm/14.11.8/etc/cgroup/release_freezer
+install -D -m755 etc/cgroup.release_common.example /opt/slurm/14.11.8/etc/cgroup/release_cpuset
+install -D -m755 etc/cgroup.release_common.example /opt/slurm/14.11.8/etc/cgroup/release_memory
+install -D -m644 etc/slurmdbd.conf.example /opt/slurm/14.11.8/etc/slurmdbd.conf.example
+install -D -m755 etc/slurm.epilog.clean /opt/slurm/14.11.8/etc/slurm.epilog.clean
+install -D -m755 contribs/sgather/sgather /opt/slurm/14.11.8/bin/sgather
+install -D -m755 contribs/sjstat /opt/slurm/14.11.8/bin/sjstat
 ##start slurmd on compute nodes and slurmctld on master node on boot
+
 ## you will need to configure file slurm.conf using the html tool in doc folder under install directory
 
 ## new gcc
