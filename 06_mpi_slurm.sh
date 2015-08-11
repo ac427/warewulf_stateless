45596d608bef3bd3957a8c23b662f2bbd0f955fd
ruilinchu <ruilinchu@gmail.com>
2015-08-11 14:32:06 -0400

    Update 06_mpi_slurm.sh

--- 06_mpi_slurm.sh
+++ 06_mpi_slurm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install rpm-build redhat-rpm-config
+yum install -y rpm-build redhat-rpm-config
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
 
