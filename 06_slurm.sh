8f7c293300680d414fa7c220f60161a950a40371
ruilinchu <ruilinchu@gmail.com>
2015-08-12 17:29:00 -0400

    Update 06_slurm.sh

--- 06_slurm.sh
+++ 06_slurm.sh
@@ -57,6 +57,15 @@ install -D -m644 etc/slurmdbd.conf.example /opt/slurm/14.11.8/etc/slurmdbd.conf
 install -D -m755 etc/slurm.epilog.clean /opt/slurm/14.11.8/etc/slurm.epilog.clean
 install -D -m755 contribs/sgather/sgather /opt/slurm/14.11.8/bin/sgather
 install -D -m755 contribs/sjstat /opt/slurm/14.11.8/bin/sjstat
+
+cat > /etc/profile.d/slurm.sh << EOF
+#!/bin/sh
+export PATH=/opt/slurm/14.11.8/bin:/opt/slurm/14.11.8/sbin:$PATH
+export LD_LIBRARY_PATH=/opt/slurm/14.11.8/lib:$LD_LIBRARY_PATH
+export MANPATH=/opt/slurm/14.11.8/share/man
+EOF
+chmod +x /etc/profile.d/slurm.sh
+cp /etc/profile.d/slurm.sh /var/chroots/centos-6/etc/profile.d/slurm.sh
 ## start slurmd on compute nodes and slurmctld on master node on boot
 
 ## you will need to configure file slurm.conf using the html tool in doc folder under install directory
