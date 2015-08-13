37d9f37ddefbd368e55f1e95c1e7bd162be142b3
ruilinchu <ruilinchu@gmail.com>
2015-08-13 17:07:14 -0400

    Update 02_ww_setup.sh

--- 02_ww_setup.sh
+++ 02_ww_setup.sh
@@ -27,5 +27,5 @@ echo @
 echo @
 echo @
 sleep 3
-wwnodescan --netdev=eth0 --ipaddr=172.16.0.1 --netmask=255.255.0.0 --vnfs=centos-6 --bootstrap=`uname -r` --groups=newnodes n[01-100]
+wwnodescan --netdev=eth0 --ipaddr=172.16.0.1 --netmask=255.255.0.0 --vnfs=centos-6 --bootstrap=`uname -r` --groups=newnodes n[01-99]
 
