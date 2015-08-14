98b7a3a64bab0b05924ba113956ade0805435618
ruilinchu <ruilinchu@gmail.com>
2015-08-14 17:34:45 -0400

    Update 06_slurm.sh

--- 06_slurm.sh
+++ 06_slurm.sh
@@ -97,16 +97,6 @@ grant all privileges on slurm_acct_db.* to 'slurm'@'localhost';
 flush privileges;
 EOF
 
-## utility to caluclate CPU usage precisely
-cat > /usr/local/bin/calc_cpu << 'EOF'
-read cpu a b c previdle rest < /proc/stat
-prevtotal=$((a+b+c+previdle))
-sleep 0.25
-read cpu a b c idle rest < /proc/stat
-total=$((a+b+c+idle))
-echo cpu `bc -l <<< "scale=2; 100*( ($total-$prevtotal) - ($idle-$previdle) ) / ($total-$prevtotal)"` % 
-EOF
-
 echo @
 echo @
 echo "you will need to edit configuration file slurm.conf and slurmdbd.conf,"
