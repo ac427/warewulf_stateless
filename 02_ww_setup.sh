828581d09665c18c1e4f53316f5e2f138da30fc8
ruilinchu <ruilinchu@gmail.com>
2015-08-14 10:03:38 -0400

    Update 02_ww_setup.sh

--- 02_ww_setup.sh
+++ 02_ww_setup.sh
@@ -3,8 +3,8 @@
 ## set passwd first time, you can omit this and go straight to wwinit ALL
 ## but later slurm will need it
 mysqladmin -u root password 'password'
-mysqladmin create warewulf -p'password'
-## or use: mysql -u root -e "create database warewulf;" -p'password'
+mysql -u root -e "create database warewulf;" -p'password'
+## or use: mysqladmin create warewulf -p'password'
 
 ## warewulf mysql set up
 sed -i '/database user/c\database user = root' /etc/warewulf/database.conf
