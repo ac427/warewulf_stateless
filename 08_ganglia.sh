016ec5fb503e5978d5d2a080106e85a1dbeabd71
ruilinchu <ruilinchu@gmail.com>
2015-08-14 15:32:33 -0400

    Update 08_ganglia.sh

--- 08_ganglia.sh
+++ 08_ganglia.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ## if you don't want to monitor master, gmond is not required
-yum -y install ganglia-gmond ganglia-gmetad ganglia-web
+yum -y install ganglia-gmond ganglia-gmetad ganglia-web bc
 chkconfig gmetad on
 chkconfig gmond on
 
 service gmond start
 service gmetad start
 
-yum --tolerant --installroot /var/chroots/centos-6 -y install ganglia-gmond
+yum --tolerant --installroot /var/chroots/centos-6 -y install ganglia-gmond bc
 
 ## edit the two /etc/ganglia/gmond.conf files
