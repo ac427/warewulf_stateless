618c1f37c38ec188b82866cdcdcacb9f4400d145
ruilinchu <ruilinchu@gmail.com>
2015-08-17 11:52:43 -0400

    Update 03_ssh_nptd.sh

--- 03_ssh_nptd.sh
+++ 03_ssh_nptd.sh
@@ -9,16 +9,9 @@ cp /etc/ssh/ssh_known_hosts /var/chroots/centos-6/etc/ssh
 
 ## install nptd to comupte nodes to sync time with master
 yum --tolerant --installroot /var/chroots/centos-6 -y install ntp
-cat > /var/chroots/centos-6/etc/ntp.conf << EOF
-driftfile /var/lib/ntp/drift
-includefile /etc/ntp/crypto/pw
-keys /etc/ntp/keys
-server 172.16.2.250 
-restrict default kod nomodify notrap nopeer noquery
-restrict -6 default kod nomodify notrap nopeer noquery
-restrict 127.0.0.1
-restrict -6 ::1
-EOF
+
+sed -i '/server /c\server 172.16.2.250' /var/chroots/centos-6/etc/ntp.conf
+
 cat >> /var/chroots/centos-6/etc/rc.local << EOF
 chkconfig ntpd on
 service ntpd start
