20f73bb5450bf75456bbdbdfa561fe3b07e7583c
ruilinchu <ruilinchu@gmail.com>
2015-08-17 11:37:08 -0400

    Update 03_ssh_nptd.sh

--- 03_ssh_nptd.sh
+++ 03_ssh_nptd.sh
@@ -11,12 +11,13 @@ cp /etc/ssh/ssh_known_hosts /var/chroots/centos-6/etc/ssh
 yum --tolerant --installroot /var/chroots/centos-6 -y install ntp
 cat > /var/chroots/centos-6/etc/ntp.conf << EOF
 driftfile /var/lib/ntp/drift
-restrict default ignore
-restrict 127.0.0.1
-server 172.16.2.250 
-restrict 172.16.2.250 nomodify
 includefile /etc/ntp/crypto/pw
 keys /etc/ntp/keys
+server 172.16.2.250 
+restrict default kod nomodify notrap nopeer noquery
+restrict -6 default kod nomodify notrap nopeer noquery
+restrict 127.0.0.1
+restrict -6 ::1
 EOF
 cat >> /var/chroots/centos-6/etc/rc.local << EOF
 chkconfig ntpd on
