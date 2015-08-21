c62ebbd810d17dcc251786b5c99de9b5719a16ce
ruilinchu <ruilinchu@gmail.com>
2015-08-21 12:40:40 -0400

    Update 08_local_scratch.sh

--- 08_local_scratch.sh
+++ 08_local_scratch.sh
@@ -5,6 +5,9 @@ pdsh -w `grep eth0 /etc/hosts | awk '{print $1}' | paste -d, -s ` "echo y | mkfs
 
 mkdir -p /var/chroots/centos-6/scratch
 
+## set the sticky bit on scratch
+chmod 1755 /var/chroots/centos-6/scratch
+
 cat >> /var/chroots/centos-6/etc/fstab << 'EOF'
 /dev/sda /scratch ext4 defaults 1 1
 EOF
