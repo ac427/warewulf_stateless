25ec8d35d3a4986116aae9f1dbf194585e661b8a
ruilinchu <ruilinchu@gmail.com>
2015-08-24 11:32:51 -0400

    Update 08_local_scratch.sh

--- 08_local_scratch.sh
+++ 08_local_scratch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-## format /dev/sda to ext4 on all compute nodes
-pdsh -w `grep eth0 /etc/hosts | awk '{print $1}' | paste -d, -s ` "echo y | mkfs.ext4 /dev/sda"
+##partition /dev/sda
+pdsh -w `grep eth0 /etc/hosts | awk '{print $1}' | paste -d, -s ` "echo -e 'n\np\n1\n1\n\nw' | fdisk /dev/sda"
+## format /dev/sda1 to ext4 on all compute nodes
+pdsh -w `grep eth0 /etc/hosts | awk '{print $1}' | paste -d, -s ` "echo y | mkfs.ext4 /dev/sda1"
 
 mkdir -p /var/chroots/centos-6/scratch
 
@@ -9,7 +11,7 @@ mkdir -p /var/chroots/centos-6/scratch
 echo "chmod 1777 /scratch" >> /var/chroots/centos-6/etc/rc.local
 
 cat >> /var/chroots/centos-6/etc/fstab << 'EOF'
-/dev/sda /scratch ext4 defaults 1 1
+/dev/sda1 /scratch ext4 defaults 1 1
 EOF
 
 ~/bin/reim_reboot
